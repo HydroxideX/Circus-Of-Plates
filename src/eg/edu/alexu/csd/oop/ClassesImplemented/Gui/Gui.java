81061de733da61c0ca696e56f3a5ac80f954f238
Youssef Alshaarawi <youssefalshaarawi@yahoo.com>
2019-12-18 01:50:51 +0200

    3ayez 23ml conflict

--- src/eg/edu/alexu/csd/oop/ClassesImplemented/Gui/Gui.java
+++ src/eg/edu/alexu/csd/oop/ClassesImplemented/Gui/Gui.java
@@ -1,17 +1,29 @@
 package eg.edu.alexu.csd.oop.ClassesImplemented.Gui;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Collections;
 
 
 public class Gui extends Application {
@@ -32,10 +44,97 @@ public class Gui extends Application {
             }
         });
         mediaPlayer.play();
+
         VBox vBox =new VBox();
-        Button play=new Button("New Game");
-        Button exit=new Button("Exit");
-        Button options=new Button("Options");
+        vBox.setSpacing(15);
+        try {
+            vBox.setBackground(
+                         new Background(null,
+                                 Collections.singletonList(new BackgroundImage(
+                                         new Image(new FileInputStream("Resources/menuBackground.png"), 300, 400, false, true),
+                                         BackgroundRepeat.NO_REPEAT,
+                                         BackgroundRepeat.NO_REPEAT,
+                                         BackgroundPosition.DEFAULT,
+                                         BackgroundSize.DEFAULT))));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        vBox.setAlignment(Pos.CENTER);
+        ImageView clown= null;
+        try {
+            clown = new ImageView(new Image(new FileInputStream("Resources/PlayB.png")));
+            clown.setFitWidth(150);
+            clown.setFitHeight(30);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Button play = new Button(null, clown);
+        DropShadow shadow = new DropShadow();
+        DropShadow finalShadow3 = shadow;
+        play.addEventHandler(MouseEvent.MOUSE_ENTERED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        play.setEffect(finalShadow3);
+                    }
+                });
+//Removing the shadow when the mouse cursor is off
+        play.addEventHandler(MouseEvent.MOUSE_EXITED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        play.setEffect(null);
+                    }
+                });
+        play.setTranslateY(-20);
+        play.setStyle("-fx-background-color: transparent;");
+        try {
+            clown = new ImageView(new Image(new FileInputStream("Resources/QuitB.png")));
+            clown.setFitWidth(150);
+            clown.setFitHeight(30);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Button exit = new Button(null, clown);
+        DropShadow finalShadow4 = shadow;
+        exit.addEventHandler(MouseEvent.MOUSE_ENTERED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        exit.setEffect(finalShadow4);
+                    }
+                });
+//Removing the shadow when the mouse cursor is off
+        exit.addEventHandler(MouseEvent.MOUSE_EXITED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        exit.setEffect(null);
+                    }
+                });
+        exit.setTranslateY(-20);
+        exit.setStyle("-fx-background-color: transparent;");
+        try {
+            clown = new ImageView(new Image(new FileInputStream("Resources/OptionsB.png")));
+            clown.setFitWidth(150);
+            clown.setFitHeight(30);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Button options = new Button(null, clown);
+        DropShadow finalShadow5 = shadow;
+        options.addEventHandler(MouseEvent.MOUSE_ENTERED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        options.setEffect(finalShadow5);
+                    }
+                });
+//Removing the shadow when the mouse cursor is off
+        options.addEventHandler(MouseEvent.MOUSE_EXITED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        options.setEffect(null);
+                    }
+                });
+
+        options.setTranslateY(-20);
+        options.setStyle("-fx-background-color: transparent;");
         play.setOnAction(e->{
             Levels levels=new Levels();
             levels.start(new Stage());
@@ -48,8 +147,9 @@ public class Gui extends Application {
             options1.start(new Stage());
         });
         vBox.getChildren().addAll(play,options,exit);
-        primaryStage.setScene(new Scene(vBox));
+        primaryStage.setScene(new Scene(vBox,300,400));
         primaryStage.setTitle("Circus of Plates");
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
     }
 }
--- src/eg/edu/alexu/csd/oop/ClassesImplemented/Gui/Options.java
+++ src/eg/edu/alexu/csd/oop/ClassesImplemented/Gui/Options.java
@@ -1,9 +1,15 @@
 package eg.edu.alexu.csd.oop.ClassesImplemented.Gui;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
@@ -11,6 +17,9 @@ import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Collections;
 
 
 public class Options extends Application {
@@ -22,12 +31,81 @@ public class Options extends Application {
     @Override
     public void start(Stage primaryStage) {
         VBox vBox=new VBox();
-        Button chooseChar=new Button("Choose Character");
+        vBox.setSpacing(10);
+        try {
+            vBox.setBackground(
+                    new Background(null,
+                            Collections.singletonList(new BackgroundImage(
+                                    new Image(new FileInputStream("Resources/menuBackground.png"), 300, 400, false, true),
+                                    BackgroundRepeat.NO_REPEAT,
+                                    BackgroundRepeat.NO_REPEAT,
+                                    BackgroundPosition.DEFAULT,
+                                    BackgroundSize.DEFAULT))));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        vBox.setAlignment(Pos.CENTER);
+        ImageView clown= null;
+        try {
+            clown = new ImageView(new Image(new FileInputStream("Resources/ChooseClown.png")));
+            clown.setFitWidth(150);
+            clown.setFitHeight(30);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Button chooseChar= new Button(null, clown);
+
+        DropShadow finalShadow2 =new DropShadow();
+        DropShadow finalShadow1 = finalShadow2;
+        DropShadow finalShadow4 = finalShadow1;
+        chooseChar.addEventHandler(MouseEvent.MOUSE_ENTERED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        chooseChar.setEffect(finalShadow4);
+                    }
+                });
+//Removing the shadow when the mouse cursor is off
+        chooseChar.addEventHandler(MouseEvent.MOUSE_EXITED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        chooseChar.setEffect(null);
+                    }
+                });
+        chooseChar.setTranslateY(-20);
+        chooseChar.setStyle("-fx-background-color: transparent;");
         chooseChar.setOnAction(e->{
             ChooseChar chooseChar1=new ChooseChar();
             chooseChar1.start(new Stage());
         });
-        Button audio =new Button("Mute Audio");
+
+
+        try {
+            clown = new ImageView(new Image(new FileInputStream("Resources/MusicB.png")));
+            clown.setFitWidth(150);
+            clown.setFitHeight(30);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Button audio= new Button(null, clown);
+
+         finalShadow2 =new DropShadow();
+         finalShadow1 = finalShadow2;
+        DropShadow finalShadow3 = finalShadow1;
+        audio.addEventHandler(MouseEvent.MOUSE_ENTERED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        audio.setEffect(finalShadow3);
+                    }
+                });
+//Removing the shadow when the mouse cursor is off
+        audio.addEventHandler(MouseEvent.MOUSE_EXITED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        audio.setEffect(null);
+                    }
+                });
+        audio.setTranslateY(-20);
+        audio.setStyle("-fx-background-color: transparent;");
         audio.setOnAction(e->{
             Gui.mediaPlayer.setMute(true);
             Gui.muteAduio= !Gui.muteAduio;
@@ -44,12 +122,39 @@ public class Options extends Application {
                 Gui.mediaPlayer.play();
             }
         });
-        Button back=new Button("Back");
+        try {
+            clown = new ImageView(new Image(new FileInputStream("Resources/BackB.png")));
+            clown.setFitWidth(150);
+            clown.setFitHeight(30);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Button back= new Button(null, clown);
+
+        finalShadow2 = new DropShadow();
+        DropShadow finalShadow = finalShadow2;
+        back.addEventHandler(MouseEvent.MOUSE_ENTERED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        back.setEffect(finalShadow);
+                    }
+                });
+//Removing the shadow when the mouse cursor is off
+        back.addEventHandler(MouseEvent.MOUSE_EXITED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        back.setEffect(null);
+                    }
+                });
+        back.setTranslateY(-20);
+        back.setStyle("-fx-background-color: transparent;");
+
         back.setOnAction(e->{
             primaryStage.close();
         });
+
         vBox.getChildren().addAll(chooseChar,audio,back);
-        primaryStage.setScene(new Scene(vBox));
+        primaryStage.setScene(new Scene(vBox,300,400));
         primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.showAndWait();
     }
